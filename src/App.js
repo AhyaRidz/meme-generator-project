311b8f7156af89154477ed820f1ceca75e6b310e
AhyaRidz <ahyariz08@gmail.com>
2023-01-14 12:50:02 +0800

    Created necessary files
    Also imported logo and custom font.
    Initialized and imported necessary stuff in different files

---
+++ src/App.js
@@ -0,0 +1,5 @@
+// ** Import
+// * React
+import React from "react";
+// * Topnav.js
+const Topnav = React.lazy(() => import("./component/Topnav"));
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// ** Import
+//  * React
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+// * CSS
+import "./index.css";
+// * App.js
+const App = React.lazy(() => import("./App"));
+
+// * div#root Reference
+const rootId = document.getElementById("root");
+const root = ReactDOM.createRoot(rootId);
